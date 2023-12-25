83884cf884dac4fb1a78e8c43eaedad722cb9c4a
Yana Kond <https://github.com/YanaKond>
2023-12-25 20:13:07 +0300

    создали первоначальный массив

--- example/Program.cs
+++ example/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+Console.WriteLine("Введите колличество строк:");
+int n = int.Parse(Console.ReadLine());
+string [] array = new string[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите строку {i + 1}: ");
+    array[i] = Console.ReadLine();
+}
+Console.Write("Первоначальный массив: ");
+Console.WriteLine(string.Join(", ", array));
